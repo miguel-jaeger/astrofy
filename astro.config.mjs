c4c09922a6d692cceb419de055b9ecde12e63b88
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-07-08 17:45:16 -0500

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miguel-jaeger.github.io',
-  base: '/astrofy',
+  base: '/curriculum',
  /* integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
